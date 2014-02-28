2507f65c7ed363641cc64ecf10d846a1e4f7ea75
Dhruva Sagar <dhruva.sagar@gmail.com>
2014-02-28 08:07:31 +0530

    New Resume using jade & less

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    jade = require('gulp-jade'),
+    uglify = require('gulp-uglify');
+
+var path = {
+  assets: {
+    less: 'app/assets/less/**/*.less'
+  },
+  views: {
+    jade: 'app/views/jade/**/*.jade'
+  }
+};
+
+gulp.task('less', function () {
+  return gulp.src(path.assets.less)
+    .pipe(less())
+    .pipe(uglify())
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('jade', function () {
+  return gulp.src(path.views.jade)
+    .pipe(jade())
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(path.views.jade, ['jade']);
+  gulp.watch(path.assets.less, ['less']);
+});
+
+gulp.task('default', ['jade', 'less', 'watch']);
