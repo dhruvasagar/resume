850ba23e61ed984ce0ed34519207f883f31ed2ae
Dhruva Sagar <dhruva.sagar@gmail.com>
2016-01-20 11:12:19 +0530

    Update resume

--- public/js/resume.js
+++ public/js/resume.js
@@ -5,4 +5,9 @@ $(document).ready(function () {
 
   var total_experience = moment("2006-08-18", "YYYY-MM-DD").fromNow(true);
   $('#total_experience').text(total_experience + ' ');
+
+  $('.collapsible').click(function() {
+    $(this).find('.collapse').toggleClass('in');
+    $(this).find('.glyphicon').toggleClass('glyphicon-triangle-bottom');
+  });
 });
